18984708cbf40fee90eb443fd26c31d682036970
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2016-03-17 10:17:30 -0500

    Fixing the forms to include the changes on the models

--- qa/forms.py
+++ qa/forms.py
@@ -1,20 +1,8 @@
-from qa.models import *
-from django.contrib.auth.models import User
+from qa.models import Question
 from django import forms
+
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ('question_text', 'tags')
-        
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')
-
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserQAProfile
-        fields = ('website', 'picture')
