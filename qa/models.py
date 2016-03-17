b4ce2b827c16f9cb00f36195c9f81987d46a359c
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2016-03-17 10:14:48 -0500

    Deleting the user implementation model, and implementing the user from the settings.AUTH_USER_MODEL for a more coherent user management

--- qa/models.py
+++ qa/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Tag(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
@@ -9,21 +10,6 @@ class Tag(models.Model):
     class Meta:
         ordering = ('slug',)
 
-from django.contrib.auth.models import User
-
-class UserQAProfile(models.Model):
-    # This line is required. Links UserQAProfile to a User model instance.
-    user = models.OneToOneField(User)
-    points = models.IntegerField(default=0)
-
-    # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='qa/static/profile_images', blank=True)
-
-    # Override the __unicode__() method to return out something meaningful!
-    def __unicode__(self):
-        return self.user.username
-
 from django_markdown.models import MarkdownField
 
 class Question(models.Model):
@@ -32,7 +18,7 @@ class Question(models.Model):
     tags = models.ManyToManyField(Tag)
     reward = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
-    user_data = models.ForeignKey(UserQAProfile)
+    user_data = models.ForeignKey(settings.AUTH_USER_MODEL)
     closed = models.BooleanField(default=False)
 
     def __str__(self):
@@ -43,22 +29,22 @@ class Answer(models.Model):
     answer_text = MarkdownField()
     votes = models.IntegerField(default=0)
     pub_date = models.DateTimeField('date published')
-    user_data = models.ForeignKey(UserQAProfile)
+    user_data = models.ForeignKey(settings.AUTH_USER_MODEL)
     def __str__(self):
         return self.answer_text
 
 class Voter(models.Model):
-    user = models.ForeignKey(UserQAProfile)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     answer = models.ForeignKey(Answer)
 
 class QVoter(models.Model):
-    user = models.ForeignKey(UserQAProfile)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     question = models.ForeignKey(Question)
 
 class Comment(models.Model):
     answer = models.ForeignKey(Answer)
     comment_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    user_data = models.ForeignKey(UserQAProfile)
+    user_data = models.ForeignKey(settings.AUTH_USER_MODEL)
     def __str__(self):
         return self.comment_text
