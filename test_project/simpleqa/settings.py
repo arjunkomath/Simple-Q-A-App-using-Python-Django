6f8d59468458f61915b509fcac6874ed3b084ce4
jose ariza <jariza@swapps.co>
2016-03-18 13:35:43 -0500

    P508Q-279 modified templates

--- test_project/simpleqa/settings.py
+++ test_project/simpleqa/settings.py
@@ -77,6 +77,28 @@ REST_FRAMEWORK = {
     ]
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            'test_project'  # insert your TEMPLATE_DIRS here
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
