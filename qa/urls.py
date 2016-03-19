6c27394dfaee8170055c24c86e5f35ab93673842
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2016-03-19 10:03:33 -0500

    Some PEPS8 to urls file

--- qa/urls.py
+++ qa/urls.py
@@ -6,13 +6,15 @@ urlpatterns = [
     url(r'^$', views.index, name='qa_index'),
     url(r'^question/(?P<question_id>\d+)/$', views.detail, name='qa_detail'),
     url(r'^answer/(?P<question_id>\d+)/$', views.answer, name='qa_answer'),
-    url(r'^new-question/$', views.CreateQuestionView.as_view(), name='qa_create_question'),
+    url(r'^new-question/$', views.CreateQuestionView.as_view(),
+        name='qa_create_question'),
     url(r'^answer/$', views.add_answer, name='add_answer'),
     url(r'^vote/(?P<user_id>\d+)/(?P<answer_id>\d+)/(?P<question_id>\d+)/(?P<op_code>\d+)/$', views.vote, name='qa_vote'),
     url(r'^comment/(?P<answer_id>\d+)/$', views.comment, name='comment'),
     url(r'^search/$', views.search, name='qa_search'),
     url(r'^tag/(?P<tag>\w+)/$', views.tag, name='qa_tag'),
-    url(r'^thumb/(?P<user_id>\d+)/(?P<question_id>\d+)/(?P<op_code>\d+)/$', views.thumb, name='thumb'),
+    url(r'^thumb/(?P<user_id>\d+)/(?P<question_id>\d+)/(?P<op_code>\d+)/$',
+        views.thumb, name='thumb'),
 
     url(r'^profile/(?P<user_id>\d+)/$', views.profile, name='qa_profile'),
 ]
