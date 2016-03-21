a15f9eaa3e543db68b84ccaa8d14e04ea1f7dbaf
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2016-03-21 09:19:07 -0500

    Adding a basic test method to be able to test the tagging functionality, due to the way django-taggit works

--- qa/tests/test_models.py
+++ qa/tests/test_models.py
@@ -5,7 +5,23 @@ from django.utils import timezone
 from qa.models import Question
 
 
-class TestModels(TestCase):
+class BasicTaggingTest(object):
+    """Defining a basic testcase method to be able to validate the tags on each
+    model record, this is needed because the way django-taggit works.
+
+    Disclaimer:
+    Taken without remorse from:
+    https://github.com/alex/django-taggit/blob/develop/tests/tests.py
+    """
+    def assert_tags_equal(self, qs, tags, sort=True, attr="name"):
+        got = [getattr(obj, attr) for obj in qs]
+        if sort:
+            got.sort()
+            tags.sort()
+        self.assertEqual(got, tags)
+
+
+class TestModels(TestCase, BasicTaggingTest):
     """TestCase class to test the models functionality
     """
 
@@ -30,3 +46,5 @@ class TestModels(TestCase):
         self.assertEqual(self.another_one.title, "Another Question")
         self.assertTrue(isinstance(self.another_one, Question))
         self.assertNotEqual(self.another_one.pub_date, timezone.now())
+        self.assert_tags_equal(self.another_one.tags.all(),
+                               ['one tag', 'the next tag', 'another tag'])
