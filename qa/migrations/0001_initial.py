5796d0e02e73740a5268b14281b769793235ca76
Cristian Vargas <cristian@swapps.co>
2016-03-29 13:37:06 -0500

    Remove migrations and bump version

--- qa/migrations/0001_initial.py
+++ qa/migrations/0001_initial.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
-import taggit.managers
+from django.db import migrations, models
 import django_markdown.models
 from django.conf import settings
+import taggit.managers
 import annoying.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0006_require_contenttypes_0002'),
         ('taggit', '0002_auto_20150616_2121'),
+        ('auth', '0006_require_contenttypes_0002'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -20,60 +20,82 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Answer',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('answer_text', django_markdown.models.MarkdownField()),
                 ('votes', models.IntegerField(default=0)),
-                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name='date published')),
+                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name=b'date published')),
             ],
         ),
         migrations.CreateModel(
-            name='Comment',
+            name='AnswerComment',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name=b'date published')),
                 ('comment_text', django_markdown.models.MarkdownField()),
-                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name='date published')),
+                ('answer', models.ForeignKey(to='qa.Answer')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='AnswerVote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('value', models.BooleanField(default=True)),
                 ('answer', models.ForeignKey(to='qa.Answer')),
             ],
         ),
         migrations.CreateModel(
             name='Question',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=200)),
+                ('votes', models.IntegerField(default=0)),
                 ('description', django_markdown.models.MarkdownField()),
-                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name='date published')),
+                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name=b'date published')),
                 ('reward', models.IntegerField(default=0)),
                 ('views', models.IntegerField(default=0)),
                 ('closed', models.BooleanField(default=False)),
-                ('tags', taggit.managers.TaggableManager(through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags', to='taggit.Tag')),
+                ('tags', taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags')),
             ],
         ),
         migrations.CreateModel(
-            name='QVoter',
+            name='QuestionComment',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name=b'date published')),
+                ('comment_text', models.CharField(max_length=250)),
+                ('question', models.ForeignKey(to='qa.Question')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='QuestionVote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('value', models.BooleanField(default=True)),
                 ('question', models.ForeignKey(to='qa.Question')),
             ],
         ),
         migrations.CreateModel(
             name='UserQAProfile',
             fields=[
-                ('user', annoying.fields.AutoOneToOneField(serialize=False, to=settings.AUTH_USER_MODEL, primary_key=True)),
+                ('user', annoying.fields.AutoOneToOneField(primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
                 ('points', models.IntegerField(default=0)),
                 ('website', models.URLField(blank=True)),
-                ('picture', models.ImageField(upload_to='qa/static/profile_images', blank=True)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Voter',
-            fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('answer', models.ForeignKey(to='qa.Answer')),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('picture', models.ImageField(upload_to=b'qa/static/profile_images', blank=True)),
             ],
         ),
         migrations.AddField(
-            model_name='qvoter',
+            model_name='questionvote',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='questioncomment',
             name='user',
             field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
         ),
@@ -83,7 +105,12 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
-            model_name='comment',
+            model_name='answervote',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='answercomment',
             name='user',
             field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
         ),
@@ -96,5 +123,13 @@ class Migration(migrations.Migration):
             model_name='answer',
             name='user',
             field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AlterUniqueTogether(
+            name='questionvote',
+            unique_together=set([('user', 'question')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='answervote',
+            unique_together=set([('user', 'answer')]),
         ),
     ]
