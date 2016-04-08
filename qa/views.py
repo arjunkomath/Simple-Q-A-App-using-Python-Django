f5eebd9fc86dba2bf26ca70f21f85ffb30871d64
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2016-04-08 15:30:28 -0500

    Deleting unnecesary code

--- qa/views.py
+++ qa/views.py
@@ -294,41 +294,6 @@ class QuestionVoteView(ParentVoteView):
     vote_model = QuestionVote
 
 
-def search(request):
-    if request.method == 'POST':
-        word = request.POST['word']
-        latest_question_list = Question.objects.filter(title__contains=word)
-        paginator = Paginator(latest_question_list, 10)
-        page = request.GET.get('page')
-        try:
-            questions = paginator.page(page)
-
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            questions = paginator.page(1)
-
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of
-            # results.
-            questions = paginator.page(paginator.num_pages)
-
-        latest_noans_list = Question.objects.order_by('-pub_date').filter(
-            tags__slug__contains=word, answer__isnull=True)[:10]
-        top_questions = Question.objects.order_by('-reward').filter(
-            tags__slug__contains=word, answer__isnull=True, reward__gte=1)[:10]
-        count = Question.objects.count
-        count_a = Answer.objects.count
-        template = loader.get_template('qa/index.html')
-        context = RequestContext(request, {
-            'questions': questions,
-            'totalcount': count,
-            'anscount': count_a,
-            'noans': latest_noans_list,
-            'reward': top_questions,
-        })
-    return HttpResponse(template.render(context))
-
-
 def profile(request, user_id):
     user_ob = get_user_model().objects.get(id=user_id)
     user = UserQAProfile.objects.get(user=user_ob)
