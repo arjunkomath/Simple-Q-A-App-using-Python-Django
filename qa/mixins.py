2ff28847075466a3bae12afc6b1959da7dfebedc
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2016-04-12 10:41:22 -0500

    Creating an authorship validation mixin to allow to update any record on the DB only by the Author of that record

--- qa/mixins.py
+++ qa/mixins.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.views.generic import View
 from django.utils.decorators import method_decorator
 
@@ -10,3 +11,13 @@ class LoginRequired(View):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(LoginRequired, self).dispatch(*args, **kwargs)
+
+
+class AuthorRequiredMixin(View):
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied
+
+        return super(
+            AuthorRequiredMixin, self).dispatch(request, *args, **kwargs)
