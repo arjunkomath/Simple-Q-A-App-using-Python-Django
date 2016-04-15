fc4118986c1c4cb48e5f5da94a97bf3fcce56cc7
Cristian Vargas <cristian@swapps.co>
2016-04-15 09:44:36 -0500

    Annotate index with the counts

--- qa/views.py
+++ qa/views.py
@@ -1,6 +1,7 @@
 import operator
 from functools import reduce
 from django.core.urlresolvers import reverse
+from django.db.models import Count
 from django.conf import settings
 from django.utils.text import slugify
 from django.utils.translation import ugettext as _
@@ -81,7 +82,10 @@ class QuestionIndexView(ListView):
         return context
 
     def get_queryset(self):
-        queryset = super(QuestionIndexView, self).get_queryset().select_related('user')
+        queryset = super(QuestionIndexView, self).get_queryset()\
+            .select_related('user')\
+            .annotate(num_answers=Count('answer'))\
+            .annotate(num_question_comments=Count('questioncomment'))
         return queryset
 
 
