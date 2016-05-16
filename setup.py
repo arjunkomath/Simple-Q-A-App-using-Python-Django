c15cd6553bc58cb106355b35b7558a6008eb6c6e
Cristian Vargas <cristian@swapps.co>
2016-05-16 11:04:18 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as file:
 
 setup(
     name='django-qa',
-    version='0.0.21',
+    version='0.0.22',
     description='Pluggable django app for Q&A',
     long_description=long_description,
     author='arjunkomath, cdvv7788, sebastian-code, jlariza, swappsco',
