d3cd1fbf5c10f55fd743a81514aa85d4e53811e2
Cristian <cristian@swapps.co>
2016-11-16 13:34:01 -0500

    Update setup.py to avoid failing with README.rst

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
 
-with open('README.md') as file:
+with open('README.rst') as file:
     long_description = file.read()
 
 setup(
