cc07eb60c6cc2f33a2f7f93a8a97be158f1a1b10
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-02-25 08:43:06 -0500

    Deleting a lot of unused code

--- qa/tests/test_models.py
+++ qa/tests/test_models.py
@@ -120,39 +120,3 @@ class TestModels(TestCase, BasicTaggingTest):
         self.assertEqual(qa_user.points, 3)
         qa_user.modify_reputation(1)
         self.assertEqual(qa_user.points, 4)
-
-# this should be tested at the views, it is not a property of the model anymore
-
-#    def test_question_positive_votes(self):
-#        QuestionVote.objects.create(user=self.user,
-#                                    value=True,  question=self.first_question)
-#        self.assertEqual(self.first_question.positive_votes, 1)
-#
-#    def test_question_negative_votes(self):
-#        QuestionVote.objects.create(user=self.user,
-#                                    value=False,  question=self.first_question)
-#        self.assertEqual(self.first_question.negative_votes, 1)
-#
-#    def test_question_total_points(self):
-#        QuestionVote.objects.create(user=self.user,
-#                                    value=True,  question=self.first_question)
-#        QuestionVote.objects.create(user=self.other_user,
-#                                    value=False, question=self.first_question)
-#        self.assertEqual(self.first_question.total_points, 0)
-
-#     def test_answer_positive_votes(self):
-#         AnswerVote.objects.create(user=self.user,
-#                                   value=True, answer=self.first_answer)
-#         self.assertEqual(self.first_answer.positive_votes, 1)
-#
-#     def test_answer_negative_votes(self):
-#         AnswerVote.objects.create(user=self.user,
-#                                   value=False, answer=self.first_answer)
-#         self.assertEqual(self.first_answer.negative_votes, 1)
-#
-#     def test_answer_total_points(self):
-#         AnswerVote.objects.create(user=self.user,
-#                                   value=True, answer=self.first_answer)
-#         AnswerVote.objects.create(user=self.other_user,
-#                                   value=False, answer=self.first_answer)
-#         self.assertEqual(self.first_answer.total_points, 0)
