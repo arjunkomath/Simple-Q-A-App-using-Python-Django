db31ffdb26395fd9a1e1ecbb986a2c74205dac5e
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-03-15 13:31:06 -0500

    Deleting the signals, now unnecesary with the new workflow

--- qa/signals.py
+++ qa/signals.py
@@ -1,69 +1,11 @@
-from django.conf import settings
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
-from .models import (Question, Answer, AnswerComment,
-                     QuestionComment, UserQAProfile)
+from .models import UserQAProfile
 
 
 @receiver(post_save, sender=User)
 def ensure_profile_exists(sender, **kwargs):
     if kwargs.get('created', False):
         UserQAProfile.objects.get_or_create(user=kwargs.get('instance'))
-
-
-@receiver(post_save, sender=Question)
-def affect_rep_at_question_creation(sender, instance, created, **kwargs):
-    # if created:
-    try:
-        points = settings.QA_SETTINGS['reputation']['CREATE_QUESTION']
-
-    except KeyError:
-        points = 2
-
-    qa_user = UserQAProfile.objects.get(user_id=instance.user)
-    qa_user.modify_reputation(points)
-
-
-@receiver(post_save, sender=Answer)
-def affect_rep_at_answer_creation(sender, instance, created, **kwargs):
-    if not created:
-        try:
-            points = settings.QA_SETTINGS['reputation']['CREATE_ANSWER']
-
-        except KeyError:
-            points = 0
-
-        qa_user = UserQAProfile.objects.get(user_id=instance.user)
-        qa_user.modify_reputation(points)
-
-
-@receiver(post_save, sender=QuestionComment)
-def affect_rep_at_questioncomment_creation(sender,
-                                           instance, created, **kwargs):
-    if not created:
-        try:
-            points = settings.QA_SETTINGS[
-                'reputation']['CREATE_QUESTION_COMMENT']
-
-        except KeyError:
-            points = 0
-
-        qa_user = UserQAProfile.objects.get(user_id=instance.user)
-        qa_user.modify_reputation(points)
-
-
-@receiver(post_save, sender=AnswerComment)
-def affect_rep_at_answercomment_creation(sender,
-                                         instance, created, **kwargs):
-    if not created:
-        try:
-            points = settings.QA_SETTINGS[
-                'reputation']['CREATE_ANSWER_COMMENT']
-
-        except KeyError:
-            points = 0
-
-        qa_user = UserQAProfile.objects.get(user_id=instance.user)
-        qa_user.modify_reputation(points)
