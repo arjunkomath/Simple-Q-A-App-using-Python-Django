63f1640907f0e9bc55cf70e6f0bf7982cea52205
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-03-17 21:32:24 -0500

    Refactoring some code, adding some style and also reestructuring the basic documentation.

--- manage.py
+++ manage.py
@@ -2,6 +2,6 @@
 import os
 from django.core import management
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
 if __name__ == "__main__":
         management.execute_from_command_line()
