a1fd00362951195a5bf916e51374740e9cff291c
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-03-17 21:01:14 -0500

    Adding the new test infraestructure because of the refactoring to the test suite, so it has a proper funtional separation.

---
+++ tests/test_mixins.py
@@ -0,0 +1,102 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+from django.test import RequestFactory, TestCase
+from django.utils import timezone
+from django.views.generic import DetailView, View
+
+from qa.mixins import AuthorRequiredMixin, LoginRequired
+from qa.models import Question
+
+
+class SomeView(LoginRequired, View):
+
+    def get(self, request):
+        return HttpResponse('something')
+
+
+class SomeAuthorRequiredView(AuthorRequiredMixin, DetailView):
+
+    model = Question
+
+    def get(self, request, pk):
+        return HttpResponse('something')
+
+
+class TestMixins(TestCase):
+    """
+    Tests functionalities for the mixins.
+    Views that implement them should only test
+    that they are subclasses of the given mixin.
+    """
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_login_required_redirects(self):
+        """
+        Anonymous users should be redirected to the LOGIN_URL
+        """
+        request = self.factory.get('some-random-place')
+        request.user = AnonymousUser()
+        response = SomeView.as_view()(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(settings.LOGIN_URL in response.url)
+
+    def test_login_request_access(self):
+        """
+        Authenticated users should be able to access the view.
+        """
+        request = self.factory.get('some-random-place')
+        request.user = get_user_model().objects.create_user(
+            username='test_user',
+            password='top_secret'
+        )
+        response = SomeView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_author_required_allow_object_user(self):
+        """
+        The owner of the object should be able to access
+        the view
+        """
+        request = self.factory.get('some-random-place')
+        request.user = get_user_model().objects.create_user(
+            username='test_user',
+            password='top_secret'
+        )
+        question = Question.objects.create(
+            title="Another Question",
+            description="A not so long random text to fill this field",
+            pub_date=timezone.datetime(2016, 1, 6, 0, 0, 0),
+            reward=0,
+            user=request.user,
+            closed=False,
+        )
+        response = SomeAuthorRequiredView.as_view()(request, pk=question.pk)
+        self.assertEqual(response.status_code, 200)
+
+    def test_author_required_not_allow_not_object_user(self):
+        """
+        A different user than the object's owner should not
+        be able to access the view, a PermissionDenied should
+        be raised
+        """
+        request = self.factory.get('some-random-place')
+        request.user = AnonymousUser()
+        user = get_user_model().objects.create_user(
+            username='test_user',
+            password='top_secret'
+        )
+        question = Question.objects.create(
+            title="Another Question",
+            description="A not so long random text to fill this field",
+            pub_date=timezone.datetime(2016, 1, 6, 0, 0, 0),
+            reward=0,
+            user=user,
+            closed=False,
+        )
+        with self.assertRaises(PermissionDenied):
+            SomeAuthorRequiredView.as_view()(
+                request, pk=question.pk)
---
+++ tests/test_settings.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8
+from __future__ import unicode_literals, absolute_import
+
+import django
+
+DEBUG = True
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+            }
+        }
+
+TIME_ZONE = 'America/Chicago'
+USE_TZ = True
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+
+
+SECRET_KEY = 'l#^#iad$8$4=dlh74$!xs=3g4jb(&j+y6*ozy&8k1-&d+vruzy'
+
+MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'django.middleware.locale.LocaleMiddleware',
+        )
+
+ROOT_URLCONF = 'qa.urls'
+
+INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'django.contrib.sites',
+        'django_markdown',
+        'taggit',
+        'qa',
+        'hitcount',
+
+        )
+
+TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'APP_DIRS': True,
+            'DIRS': [],
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.template.context_processors.i18n',
+                    'django.template.context_processors.media',
+                    'django.template.context_processors.static',
+                    'django.template.context_processors.tz',
+                    'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = '/foo/list/'
+
+QA_SETTINGS = {
+    'qa_messages': True,
+    'qa_description_optional': False,
+    'reputation': {
+        'CREATE_QUESTION': 0,
+        'CREATE_ANSWER': 0,
+        'CREATE_ANSWER_COMMENT': 0,
+        'CREATE_QUESTION_COMMENT': 0,
+        'ACCEPT_ANSWER': 0,
+        'UPVOTE_QUESTION': 0,
+        'UPVOTE_ANSWER': 0,
+        'DOWNVOTE_QUESTION': 0,
+        'DOWNVOTE_ANSWER': 0,
+    }
+}
