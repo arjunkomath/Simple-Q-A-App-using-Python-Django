1010939b9ab383d055340cdfd3944c8f5c4049ea
Cristian Vargas <cristian@swapps.co>
2018-09-20 20:16:25 -0500

    Update description in setup.py

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
     ],
     install_requires=get_requirements(),
     extras_require={
